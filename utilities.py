f6d53c4aa52b4d8926ccab4059a93a808c7d48e4
Sebastian <sebastian.mettes@gmail.com>
2022-11-02 13:39:55 -0400

    utilities updates

--- utilities.py
+++ utilities.py
@@ -6,12 +6,18 @@ from tqdm.auto import tqdm
 
 
 class DL_Utilities():
-    def __init__(self):
+    def __init__(self,samples=False,epochs=False):
         self.data = np.genfromtxt("/data/sim/agent/data.csv",delimiter=",")
         self.trial = "/data/sim/trial"
         self.agent = "/data/sim/agent"
-        self.samples = [10,20,30,40,50,60,70,80,90,100] #Samples used for course plotting
-
+        if samples:
+            self.samples = samples
+        else:
+            self.samples = [10,20,30,40,50,60,70,80,90,100] #Samples used for course plotting
+        if epochs:
+            self.epochs = epochs
+        else:
+            self.epochs = [1,100,200,300,400,500]
     def loss(self):
         plt.plot(self.data[:,0],self.data[:,2])
         plt.xlabel('Epoch Number')
@@ -25,6 +31,8 @@ class DL_Utilities():
         plt.show()
 
     def collate_data(self):
+        file_list = [name for name in os.listdir(self.trial) if os.path.isfile(os.path.join(self.trial,name))]
+        
         pass
 
     def position_plot(self):
@@ -33,7 +41,7 @@ class DL_Utilities():
     def velocity_plot(self):
         pass
 
-    def joint_angle_plot(self,joint_num = 0)
+    def joint_angle_plot(self,joint_num = 0, )
 
 
 if __name__ == "__main__":
