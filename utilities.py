1beb945d2b46ed572b4bb5845ea117c1d5ed9426
Sebastian <sebastian.mettes@gmail.com>
2022-11-02 12:20:07 -0400

    started a utilities script

---
+++ utilities.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import json
+from tqdm.auto import tqdm
+
+
+class DL_Utilities():
+    def __init__(self):
+        pass
+
+    def loss_plot(self):
+        pass
+
+    def reward_plot(self):
+        pass
+
+    def position_plot(self):
+        pass
+
+    def velocity_plot(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
