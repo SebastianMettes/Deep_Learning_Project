04a5b7faf51280851df8712681692daa1d3b1ea4
Sebastian <sebastian.mettes@gmail.com>
2022-11-20 14:31:32 -0500

    initial PPO setup, in progress, check config file if running non PPO network

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 from agentLib import MLP_agent
+from agentLib import PPO_agent
 import datetime
 import os
 import json
@@ -49,3 +50,47 @@ class env():
                 if terminated or truncated:
                     observation, info = self.session.reset()
                     break
+
+class env_PPO():
+    def __init__(self,config,host_id):
+        self.config = config
+        self.num_steps = config['num_steps']
+        self.host_id = host_id
+
+    def launch(self):
+        self.session = gym.make('Ant-v4',exclude_current_positions_from_observation=False)
+        agent = PPO_agent(self.config)
+
+
+        while True:
+            now = datetime.now()
+            date_time = now.strftime("%Y%m%d%H%M%S")
+
+            agent_version = agent.update_version()
+            filename = str(self.host_id)+"."+date_time+".JSON"
+            filename = os.path.join(self.config["save_dir"],str(agent_version),filename)
+
+            observation, info = self.session.reset()
+            
+            action = [0,0,0,0,0,0,0,0] #Initialize 0-action as starting condition            
+            observation = np.append(observation,action)
+            observation = torch.tensor(observation)
+            state_tensor = []
+
+            for i in range(self.num_steps):
+                action_old = action
+                action,digit = agent.calc_action(agent_version,observation,action)
+                observation_new, reward, terminated, truncated, info = self.session.step(action)
+
+                observation_new = np.append(observation_new,action)
+
+                state_tensor.append((observation.tolist(),observation_new.tolist(),digit,reward))
+
+                with open(filename,"w") as file:
+                    file.write(json.dumps(state_tensor,indent=0))
+                observation_new = torch.tensor(observation_new)
+                observation = observation_new
+                if terminated or truncated:
+                    observation, info = self.session.reset()
+                    break
+
--- slave_node.py
+++ slave_node.py
@@ -8,6 +8,7 @@ import json
 import os
 import uuid
 from env import env
+from env import env_PPO
 #notes:
 '''
 To Launch the agent, the config file must icnlude all the environment and agent parameters. Make sure master node is running first.
@@ -22,5 +23,9 @@ with open("/data/sim/config.json","r") as file:
     config=json.load(file)
 
 #Set number of simulation steps per simulation:
-Ant = env(config,host_id)
+
+if config["NETWORK"] == "PPO":
+    Ant = env_PPO(config,host_id)
+else: 
+    Ant = env(config,host_id)
 Ant.launch()
