57fa2cb6c944fad0cad2eaa4973413b4183b20bf
Sebastian <sebastian.mettes@gmail.com>
2022-11-20 15:52:44 -0500

    PPO separate folder.

--- agentLib.py
+++ agentLib.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim import Adam
 from torch.distributions import MultivariateNormal
+from PPO.network import FeedForwardNN
 import time
 
 
@@ -62,112 +63,6 @@ class MLP_agent(nn.Module):
             self.cuda()
         else:
             self.cpu()
-
-
-    def cuda(self):
-        self.net = self.net.cuda()
-    def cpu(self):
-        self.net = self.net.cpu()
-
-    def forward(self,observation):
-        return self.net(observation)
-
-    def calc_action(self,agent_version,state,torque_input):
-        if self.version!=agent_version:
-            self.net.load_model(os.path.join(self.agent_path,str(agent_version),'model.pt'))
-        
-        act = state.float().unsqueeze(0)
-        action_probability = self.net.forward(act)
-        action_probability = nn.functional.softmax(action_probability,dim=1)
-        action_probability = action_probability.data.numpy()[0]
-        action = np.random.choice(len(action_probability),p=action_probability) #return an action based on softmax probability that action is most likely to produce positive reward
-        
-        #Create output options array:
-        a0 = np.array([-1,1])
-        a1 = np.array([-1,1])
-        a2 = np.array([-1,1])
-        a3 = np.array([-1,1])
-        a4 = np.array([-1,1])
-        a5 = np.array([-1,1])
-        a6 = np.array([-1,1])
-        a7 = np.array([-1,1])
-        comb_array = np.array(np.meshgrid(a0,a1,a2,a3,a4,a5,a6,a7)).T.reshape(-1,8)
-        output = torque_input+comb_array[action,:]*self.torque_multiplier
-        return output,action
-
-    def load_model(self,version_path):
-        self.net.load_state_dict(torch.load(version_path))
-
-    def save_model(self,directory_path,optimizer):
-        tmp_name = directory_path+'_tmp'
-        if os.path.isdir(tmp_name) == False:        
-            os.mkdir(tmp_name, mode = 0o777)
-
-        torch.save(self.net.state_dict(),os.path.join(tmp_name,'model.pt'))
-        torch.save(optimizer.state_dict(),os.path.join(tmp_name,'optimizer.pt'))
-        shutil.move(tmp_name,directory_path)
-
-    def update_version(self):
-        i = 1
-        while os.path.isdir(os.path.join(self.agent_path,str(i))):
-            i+=1
-        i-=1
-        if i>self.version:
-            time.sleep(0.1)
-            print('loading version ',i)
-            self.load_model(os.path.join(self.agent_path,str(i),'model.pt'))
-            self.version = i
-        return(i)
-
-######################################################## PPO_AGENT:
-class PPO_agent(nn.Module):
-    def __init__(self,agent_config):
-        super().__init__()
-        self.agent_config = agent_config
-        self.HIDDEN_SIZE = agent_config['HIDDEN_SIZE']
-        self.OBSERVE_SIZE = agent_config['OBSERVE_SIZE']
-        self.N_ACTIONS = agent_config['N_ACTIONS']
-        self.BATCH_SIZE = agent_config['BATCH_SIZE']
-        self.PERCENTILE = agent_config['PERCENTILE']        
-        self.agent_path = agent_config['agent_path']
-        self.torque_multiplier = agent_config['torque_multiplier']
-        self.NUM_CONT_ACTIONS = agent_config['num_cont_actions']
-        self.version = 0
-        self.cuda_test = agent_config['cuda']
-        self.initialize()
-        if os.path.isfile(os.path.join(self.agent_path,str(1),'model.pt')):
-            self.update_version()
-
-
-
-
-    def initialize(self):
-        self.obs_dim = self.OBSERVE_SIZE
-        hidden_size = self.HIDDEN_SIZE
-        self.act_dim = self.NUM_CONT_ACTIONS
-
-        self.actor = policy_class(self.obs_dim,self.act_dim)
-        self.critic = policy_class(self.obs_dim,1)
-
-        self.actor_optim = Adam(self.actor.parameters(),lr = self.lr)
-
-        self.net = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size,hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size,n_actions),
-            nn.ReLU(),
-            nn.Linear(n_actions,n_actions),
-            nn.ReLU(),
-            nn.Linear(n_actions,n_actions),
-        )
-        if self.cuda_test == True:
-            self.cuda()
-        else:
-            self.cpu()
-
-
 
 
     def cuda(self):
