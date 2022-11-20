fc06c11b0a11a75d4ef55d98bdd126df98fa76fa
Sebastian <sebastian.mettes@gmail.com>
2022-11-20 18:27:18 -0500

    increased PPO linear layer depths

--- PPO/network.py
+++ PPO/network.py
@@ -24,8 +24,10 @@ class FeedForwardNN(nn.Module):
 		super(FeedForwardNN, self).__init__()
 
 		self.layer1 = nn.Linear(in_dim, 64)
-		self.layer2 = nn.Linear(64, 64)
-		self.layer3 = nn.Linear(64, out_dim)
+		self.layer2 = nn.Linear(128, 256)
+		self.layer3 = nn.Linear(256, 256)
+		self.layer4 = nn.Linear(256,128)
+		self.layer5 = nn.Linear(128,out_dim)
 
 	def forward(self, obs):
 		"""
@@ -41,6 +43,9 @@ class FeedForwardNN(nn.Module):
 
 		activation1 = F.relu(self.layer1(obs))
 		activation2 = F.relu(self.layer2(activation1))
-		output = self.layer3(activation2)
+		activation3 = F.relu(self.layer3(activation2))
+		activation4 = F.relu(self.layer4(activation3))
+	
+		output = self.layer5(activation4)
 
 		return output
