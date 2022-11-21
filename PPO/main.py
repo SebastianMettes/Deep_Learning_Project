cde0845de8c8a35c34c69f3a0c8bbe30bf833b99
Sebastian <sebastian.mettes@gmail.com>
2022-11-20 20:11:39 -0500

    lower learning rate

--- PPO/main.py
+++ PPO/main.py
@@ -92,7 +92,7 @@ def main(args):
 				'max_timesteps_per_episode': 100, 
 				'gamma': 0.99, 
 				'n_updates_per_iteration': 10,
-				'lr': 3e-3, 
+				'lr': 3e-5, 
 				'clip': 0.2,
 				'render': True,
 				'render_every_i': 10
